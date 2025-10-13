8705de336cfd498e6bd86fe88656c5a270d1160b
Soviet Cat <harrison.laine@outlook.com>
2025-10-13 22:29:06 +1000

    Initial Build System
    Added CMakeLists.txt - Initial Build System (KratosRuntime (no install) and KratosBuilder)
    Changed .gitignore
    Added builder/private/test.cpp - Test Build
    Added builder/public/KB_test.hpp - Test Build
    Added runtime/private/test.cpp - Test Build
    Added runtime/public/KR_test.hpp - Test Build

---
+++ builder/private/test.cpp
@@ -0,0 +1,8 @@
+#include <print>
+
+#include "KR_test.hpp"
+
+int main(int argc, char* argv[]) {
+    printHelloWorld();
+    return 0;
+}
---
+++ runtime/private/test.cpp
@@ -0,0 +1,6 @@
+#include "KR_test.hpp"
+#include <print>
+
+void printHelloWorld() {
+    std::print("Hello world!\n");
+}
